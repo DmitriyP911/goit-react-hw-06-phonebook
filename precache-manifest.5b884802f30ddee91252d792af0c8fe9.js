83ce190ee004e91b6eaa9acff5119f2223cef450
Dmitriy Pismenniy <dima30655@gmail.com>
2020-06-30 12:51:10 +0300

    Updates

---
+++ precache-manifest.5b884802f30ddee91252d792af0c8fe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506a09c6a34825456efa1d97c9543f49",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "013281e9403ac24f4152",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.17df1663.chunk.css"
+  },
+  {
+    "revision": "487755d4655f05a63407",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.fd97ab66.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.fd97ab66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "013281e9403ac24f4152",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.e4ef43ff.chunk.js"
+  },
+  {
+    "revision": "86e9a7fe0c1479afab58",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.2d3ded43.js"
+  }
+]);
